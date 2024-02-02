4f88e3fad75a91af4a9c3ed705ce1017865deaa5
mohitsingla1992 <58739094+mohitsingla1992@users.noreply.github.com>
2024-02-02 15:22:05 +0530

    Update untitled79.py

--- untitled79.py
+++ untitled79.py
@@ -10,8 +10,8 @@ Original file is located at
 import pandas as pd
 
 # Replace 'your_gcs_path' with the actual path to your dataset on Google Cloud Storage
-#gcs_path = 'gs://your-bucket-name/your-dataset.csv'
-gcs_path =gs://cloud-ai-platform-473b9f9a-187c-4d55-abe8-c08eeef28beb/heart_stroke_data/healthcare-dataset-stroke-data.csv
+gcs_path = "https://storage.cloud.google.com/mohit_123/train.csv"
+#gcs_path =gs://cloud-ai-platform-473b9f9a-187c-4d55-abe8-c08eeef28beb/heart_stroke_data/healthcare-dataset-stroke-data.csv
 
 # Load the dataset into a pandas DataFrame
 df = pd.read_csv(gcs_path)
